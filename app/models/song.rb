ac188c9417fd72e661ac711b831bb00e9dceefcb
Daniel Amyakar <dan.rockit@gmail.com>
2020-06-30 17:26:09 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,8 +7,6 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    drake = Artist.new{name = "Drake"}
-    self.artist = drake
-    self
+    self.artist = Artist.find_or_create_by(name: "Drake")
   end
 end
